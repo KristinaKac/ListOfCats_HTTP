454b543b71ba0acb202559149a78c2d2974b61cd
KristinaKac <kachalkovakristina@mailru>
2022-07-29 18:36:26 +0300

    First Commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        CloseableHttpResponse response = httpClient.execute(request);
+        List<Post> posts = mapper.readValue(
+                response.getEntity().getContent(), new TypeReference<List<Post>>() {});
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+        Stream<Post> stream = posts.stream()
+                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0);
+        stream.forEach(System.out::println);
+
+    }
+}
